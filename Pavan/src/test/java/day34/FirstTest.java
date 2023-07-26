9534e53d2dd581cf685c84f0de843e0fc862f3c9
Satishlk121 <satishlk121@gmail.com>
2023-07-26 17:05:54 +0530

    first commit

---
+++ Pavan/src/test/java/day34/FirstTest.java
@@ -0,0 +1,23 @@
+package day34;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+  @Test
+  public void Test1()
+  {
+	  System.out.println("Testing1");
+	  
+  }
+  @Test
+  public void Test2()
+  {
+	  System.out.println("Testing 2");
+	  
+  }
+  @Test
+  public void Test3()
+  {
+	  System.out.println("Testing3");
+  }
+}
